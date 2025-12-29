b2962044d7c3540dd63985d89efb5e46376f3c1e
ady1029 <adrianernestorodriguez2910@gmail.com>
2025-12-28 22:49:19 -0500

    add

---
+++ src/components/Navbar.vue
@@ -0,0 +1,118 @@
+<template>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark fixed-top">
+    <div class="container-fluid">
+      <div>
+        <h1 class="navbar-brand mb-0">AutoNova</h1>
+      </div>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navPanel"
+        aria-controls="navPanel"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navPanel">
+        <div class="navegacion">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/" @click="cerrarPanel">Inicio</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/" @click="cerrarPanel">Servicios</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/" @click="cerrarPanel">Quienes somos?</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/" @click="cerrarPanel">Contacto</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/" @click="cerrarPanel">FAQ</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import '@/assets/bootstrap.bundle'
+function cerrarPanel() {
+  const navPanel = document.getElementById('navPanel')
+  const bsCollapse = bootstrap.Collapse.getInstance(navPanel)
+  if (bsCollapse) {
+    bsCollapse.hide()
+  }
+}
+
+function cerrarSesion() {
+  localStorage.removeItem('token')
+  cerrarPanel()
+  router.push('/')
+}
+</script>
+<style scoped>
+* {
+  font-family: Inter, sans-serif;
+}
+button {
+  border: none;
+  box-shadow: none;
+}
+nav {
+  font-size: 1rem;
+  gap: 1rem;
+  background-color: #1f3a5f;
+  padding: 0;
+}
+.navegacion {
+  background-color: #2d7a5c;
+  padding: 0.5rem;
+}
+
+@media (min-width: 992px) {
+  .navegacion {
+    background-color: #1f3a5f;
+    padding: 0;
+  }
+}
+nav a {
+  display: inline-block;
+  padding: 0 0.5rem;
+  border-left: 1px solid black;
+  text-decoration: none;
+  color: white;
+}
+
+nav a:first-of-type {
+  border-left: none;
+  margin: 0;
+}
+
+nav a.router-link-exact-active {
+  color: #f5a623;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+h1 {
+  margin-left: 1rem;
+  font-size: 1.5rem;
+  color: white;
+}
+
+.nav-link.router-link-exact-active {
+  font-weight: bold;
+  text-decoration: underline;
+}
+li {
+  margin-top: 0.5rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <div class="main d-flex align-items-center justify-content-center px-3">
+      <div class="container">
+        <h1 class="fw-bold text-white mb-3">Soluciones Tecnologicas Integrales</h1>
+        <p>Automatización, electrónica, electricidad y climatización para tu industria</p>
+        <div class="d-flex flex-wrap justify-content-center">
+          <a href="#servicios" class="btn btn-yellow fw-semiblod"> Explorar Servicios </a>
+          <a href="#contacto" class="btn btn-outline-light fw-semibold"> Contactar </a>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<script setup></script>
+
+<style scoped>
+.main {
+  background-image: url(../components/icons/fondo-2.jpg);
+  background-attachment: fixed;
+  background-position: center top;
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  color: white;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+h1 {
+  font-size: 2.8rem;
+  text-align: center;
+}
+p {
+  font-size: 1.5rem;
+  width: 100%;
+  text-align: center;
+}
+a {
+  margin-top: 1rem;
+  text-align: center;
+  height: 4rem;
+  font-size: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0;
+  padding-left: 0.3rem;
+}
+.btn-yellow {
+  background-color: #f5a623;
+  margin-right: 2rem;
+  color: #1f3a5f;
+  width: 7rem;
+}
+.btn-yellow:hover {
+  background-color: #ffca2b;
+}
+</style>
