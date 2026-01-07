b61586a049fff2fe67a91bf9fbb90a3e7fa36b2b
ady1029 <adrianernestorodriguez2910@gmail.com>
2026-01-06 21:04:40 -0500

    add

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import vitePluginSitemap from 'vite-plugin-sitemap'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { hostname } from 'node:os'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+   vitePluginSitemap({
+  hostname: 'https://autonovacorp.netlify.app',
+})
   ],
   resolve: {
     alias: {
